7ef680c5b8926ab7a07dad8ae7bf4654e4f0b727
JJH636 <1506962645@qq.com>
2020-01-11 10:07:46 +0800

    测试1

---
+++ src/main/java/entity/T.java
@@ -0,0 +1,13 @@
+package entity;
+
+public class T {
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    private String id;
+}
