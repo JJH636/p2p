d88956e8f8e0d705f9f5e7261d75f4cf86018a24
JJH636 <54616379+JJH636@users.noreply.github.com>
2020-02-11 15:56:23 +0800

    Delete UserAccountMapper.java

--- src/main/java/com/baby/p2p/dao/user/UserAccountMapper.java
+++
@@ -1,6 +0,0 @@
-package com.baby.p2p.dao.user;
-
-import com.baby.p2p.pojo.UserAccount;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-
-public interface UserAccountMapper extends BaseMapper<UserAccount> { }
